c4f6a80e874a42f4ed36425e7b8216a03d180a3a
Theo MULLER <theo.muller@labom2iformation.fr>
2026-03-06 16:47:40 +0100

    affinage du style, formulaire d'inscription et de connexion gérés + rate-limiter sur route /login et /register

--- server/src/router/routes/authRoutes.js
+++ server/src/router/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import { AuthController } from "../../controllers/AuthController.js"
 import { Router } from "express"
+import { rateLimiter } from "../../middlewares/rateLimiter.js"
 
 export const authRoutes = Router()
 
 // routes publiques : accessibles sans token
-authRoutes.post("/register", AuthController.register)
-authRoutes.post("/login", AuthController.login)
+authRoutes.post("/register", rateLimiter, AuthController.register)
+authRoutes.post("/login", rateLimiter, AuthController.login)
 authRoutes.post("/logout", AuthController.logout)
 authRoutes.post("/refresh-token", AuthController.refreshToken)
 
